9744a810ca354b4760c3a7270fee188b0ec3d5e6
David Petnick <dpetnick89@gmail.com>
2020-05-31 22:40:39 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -1,17 +1,4 @@
-class TicTacToe
-  def initialize(board = [" "," "," "," "," "," "," "," "," "])
-    @board = board
-  end
-
-  def board=(board)
-    @board = board
-  end
-
-  def board
-    @board
-  end
-
-  WIN_COMBINATIONS = [
+WIN_COMBINATIONS = [
   [0,1,2],
   [3,4,5],
   [6,7,8],
@@ -19,85 +6,113 @@ class TicTacToe
   [1,4,7],
   [2,5,8],
   [0,4,8],
-  [6,4,2]
+  [2,4,6]
 ]
-  def display_board()
-    puts " #{@board[0]} | #{@board[1]} | #{@board[2]} "
-    puts "-----------"
-    puts " #{@board[3]} | #{@board[4]} | #{@board[5]} "
-    puts "-----------"
-    puts " #{@board[6]} | #{@board[7]} | #{@board[8]} "
+
+def display_board(board)
+  puts " #{board[0]} | #{board[1]} | #{board[2]} "
+  puts "-----------"
+  puts " #{board[3]} | #{board[4]} | #{board[5]} "
+  puts "-----------"
+  puts " #{board[6]} | #{board[7]} | #{board[8]} "
+end
+
+def move(board, location, current_player = "X")
+  board[location.to_i-1] = current_player
+end
+
+def position_taken?(board, location)
+  board[location] != " " && board[location] != ""
+end
+
+def valid_move?(board, position)
+  position.to_i.between?(1,9) && !position_taken?(board, position.to_i-1)
+end
+
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets.strip
+  if valid_move?(board, input)
+    move(board, input, current_player(board))
+  else
+    turn(board)
   end
+  display_board(board)
+end
 
-  def position(location)
-    @board[location.to_i]
+def turn_count(board)
+  counter = 0
+  board.each do |i|
+    if i == "X" || i == "O"
+      counter += 1
+    end
   end
+  return counter
+end
 
-  def move(location, current_player = "X")
-    @board[location.to_i-1] = current_player
-  end
+def current_player(board)
+  turn_count(board) % 2 == 0 ? "X" : "O"
+end
 
-  def current_player
-    turn_count % 2 == 0 ? "X"  : "O"
-  end
+def won?(board)
 
+  WIN_COMBINATIONS.detect do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-  def position_taken?(location)
-    !(position(location) == nil || position(location) == " ")
-  end
+    position_1 = board[win_index_1]
+    position_2 = board[win_index_2]
+    position_3 = board[win_index_3]
 
-  def valid_move?(input)
-    input.to_i.between?(1,9) && !position_taken?(input.to_i - 1)
-  end
-
-  def turn
-    puts "Please enter 1-9"
-    input = gets.strip
-    if !valid_move?(input)
-      turn
+    if position_1 == "X" && position_2 == "X" && position_3 == "X"
+      return win_combination
+    elsif position_1 == "O" && position_2 == "O" && position_3 == "O"
+      return win_combination
     else
-      move(input, current_player)
-    end
-    display_board
-  end
-
-  def turn_count
-    @board.count{|i| i == "X" || i == "O"}
-  end
-
-  def won?
-    WIN_COMBINATIONS.detect do |i|
-    position(i[0]) == position(i[1]) && position(i[1]) == position(i[2]) && position_taken?(i[0])
+      false
     end
   end
+end
 
-  def full?
-    @board.all? {|i| i == "X" || i == "O"}
+def full?(board)
+  if board.detect {|i| i == " " || i == nil}
+    return false
+  else
+    return true
   end
+end
 
-  def draw?
-    !won? && full?
+def draw?(board)
+  if !won?(board) && full?(board)
+    return true
+  else
+    return false
   end
+end
 
-  def over?
-    won? || draw?
+def over?(board)
+  if draw?(board) || won?(board)
+    return true
+  else
+    return false
   end
+end
 
-  def winner
-    if i = won?
-      @winner = position(i.first)
-    end
+def winner(board)
+  if win_combination = won?(board)
+    board[win_combination.first]
   end
+end
 
-  def play
-    while !over?
-      turn
-    end
-
-    if won?
-      puts "You did it #{winner}! You Won!"
-    elsif draw?
-      puts "A Tie!"
-    end
+def play(board)
+  until over?(board)
+    current_player(board)
+    turn(board)
+  end
+  if won?(board)
+    puts "You did it #{winner(board)}! You Won!"
+  else
+    puts "A Tie!"
   end
 end
